f56dbdce8c768f486f9e4460d01930d350e13f42
Nicolas Vazquez <81201032+nicvazquez@users.noreply.github.com>
2023-02-17 16:13:13 -0300

    stash

---
+++ src/data/user.ts
@@ -0,0 +1,8 @@
+import { UserInterface } from "@/interfaces/UserInterface";
+
+export const user: UserInterface = {
+	name: "Nicolás Vazquez",
+	avatar: "/avatar.webp",
+	role: "Software Developer",
+	company: "AppDirect",
+};
---
+++ src/interfaces/UserInterface.ts
@@ -0,0 +1,6 @@
+export interface UserInterface {
+	name: string;
+	avatar: string;
+	role: string;
+	company?: string;
+}
